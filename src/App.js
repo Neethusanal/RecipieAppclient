34796c3d220dd5a06f0ee1e41b4a48056e76d11f
Neethu Sanal Kumar <117423137+Neethusanal@users.noreply.github.com>
2024-05-28 00:12:56 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
-import './App.css';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home';
+import Layout from './Layouts/Layout';
 
 function App() {
   return (
     <div className="App">
     <Router>
       <Routes>
+        <Route element={<Layout/>}/>
         <Route path="/" element={<Home/>}></Route>
       </Routes>
     </Router>
